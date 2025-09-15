3772ec9325d5787099c1651f14e64442383689b9
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-09-15 13:18:18 -0400

    Update ABIs

--- src/abi/vault.ts
+++ src/abi/vault.ts
@@ -5,24 +5,6 @@ export const ABI = [
     "interface_name": "pitch_lake::vault::interface::IVault"
   },
   {
-    "type": "enum",
-    "name": "pitch_lake::vault::interface::VaultType",
-    "variants": [
-      {
-        "name": "InTheMoney",
-        "type": "()"
-      },
-      {
-        "name": "AtTheMoney",
-        "type": "()"
-      },
-      {
-        "name": "OutOfMoney",
-        "type": "()"
-      }
-    ]
-  },
-  {
     "type": "struct",
     "name": "core::integer::u256",
     "members": [
@@ -47,88 +29,9 @@ export const ABI = [
     ]
   },
   {
-    "type": "struct",
-    "name": "pitch_lake::fossil_client::interface::L1Data",
-    "members": [
-      {
-        "name": "twap",
-        "type": "core::integer::u256"
-      },
-      {
-        "name": "volatility",
-        "type": "core::integer::u128"
-      },
-      {
-        "name": "reserve_price",
-        "type": "core::integer::u256"
-      }
-    ]
-  },
-  {
-    "type": "struct",
-    "name": "pitch_lake::fossil_client::interface::RoundSettledReturn",
-    "members": [
-      {
-        "name": "total_payout",
-        "type": "core::integer::u256"
-      }
-    ]
-  },
-  {
-    "type": "enum",
-    "name": "pitch_lake::fossil_client::interface::FossilCallbackReturn",
-    "variants": [
-      {
-        "name": "RoundSettled",
-        "type": "pitch_lake::fossil_client::interface::RoundSettledReturn"
-      },
-      {
-        "name": "FirstRoundInitialized",
-        "type": "()"
-      }
-    ]
-  },
-  {
-    "type": "struct",
-    "name": "pitch_lake::types::Bid",
-    "members": [
-      {
-        "name": "bid_id",
-        "type": "core::felt252"
-      },
-      {
-        "name": "owner",
-        "type": "core::starknet::contract_address::ContractAddress"
-      },
-      {
-        "name": "amount",
-        "type": "core::integer::u256"
-      },
-      {
-        "name": "price",
-        "type": "core::integer::u256"
-      },
-      {
-        "name": "tree_nonce",
-        "type": "core::integer::u64"
-      }
-    ]
-  },
-  {
     "type": "interface",
     "name": "pitch_lake::vault::interface::IVault",
     "items": [
-      {
-        "type": "function",
-        "name": "get_vault_type",
-        "inputs": [],
-        "outputs": [
-          {
-            "type": "pitch_lake::vault::interface::VaultType"
-          }
-        ],
-        "state_mutability": "view"
-      },
       {
         "type": "function",
         "name": "get_alpha",
@@ -164,11 +67,22 @@ export const ABI = [
       },
       {
         "type": "function",
-        "name": "get_fossil_client_address",
+        "name": "get_verifier_address",
         "inputs": [],
         "outputs": [
           {
             "type": "core::starknet::contract_address::ContractAddress"
+          }
+        ],
+        "state_mutability": "view"
+      },
+      {
+        "type": "function",
+        "name": "get_deployment_block",
+        "inputs": [],
+        "outputs": [
+          {
+            "type": "core::integer::u64"
           }
         ],
         "state_mutability": "view"
@@ -229,6 +143,28 @@ export const ABI = [
         "outputs": [
           {
             "type": "core::starknet::contract_address::ContractAddress"
+          }
+        ],
+        "state_mutability": "view"
+      },
+      {
+        "type": "function",
+        "name": "get_program_id",
+        "inputs": [],
+        "outputs": [
+          {
+            "type": "core::felt252"
+          }
+        ],
+        "state_mutability": "view"
+      },
+      {
+        "type": "function",
+        "name": "get_proving_delay",
+        "inputs": [],
+        "outputs": [
+          {
+            "type": "core::integer::u64"
           }
         ],
         "state_mutability": "view"
@@ -370,7 +306,7 @@ export const ABI = [
       },
       {
         "type": "function",
-        "name": "get_request_to_settle_round",
+        "name": "get_request_to_start_first_round",
         "inputs": [],
         "outputs": [
           {
@@ -381,7 +317,7 @@ export const ABI = [
       },
       {
         "type": "function",
-        "name": "get_request_to_start_first_round",
+        "name": "get_request_to_settle_round",
         "inputs": [],
         "outputs": [
           {
@@ -456,26 +392,6 @@ export const ABI = [
       },
       {
         "type": "function",
-        "name": "fossil_client_callback",
-        "inputs": [
-          {
-            "name": "l1_data",
-            "type": "pitch_lake::fossil_client::interface::L1Data"
-          },
-          {
-            "name": "timestamp",
-            "type": "core::integer::u64"
-          }
-        ],
-        "outputs": [
-          {
-            "type": "pitch_lake::fossil_client::interface::FossilCallbackReturn"
-          }
-        ],
-        "state_mutability": "external"
-      },
-      {
-        "type": "function",
         "name": "start_auction",
         "inputs": [],
         "outputs": [
@@ -498,87 +414,15 @@ export const ABI = [
       },
       {
         "type": "function",
-        "name": "place_bid",
+        "name": "fossil_callback",
         "inputs": [
           {
-            "name": "amount",
-            "type": "core::integer::u256"
+            "name": "job_request",
+            "type": "core::array::Span::<core::felt252>"
           },
           {
-            "name": "price",
-            "type": "core::integer::u256"
-          }
-        ],
-        "outputs": [
-          {
-            "type": "pitch_lake::types::Bid"
-          }
-        ],
-        "state_mutability": "external"
-      },
-      {
-        "type": "function",
-        "name": "update_bid",
-        "inputs": [
-          {
-            "name": "bid_id",
-            "type": "core::felt252"
-          },
-          {
-            "name": "price_increase",
-            "type": "core::integer::u256"
-          }
-        ],
-        "outputs": [
-          {
-            "type": "pitch_lake::types::Bid"
-          }
-        ],
-        "state_mutability": "external"
-      },
-      {
-        "type": "function",
-        "name": "refund_unused_bids",
-        "inputs": [
-          {
-            "name": "round_address",
-            "type": "core::starknet::contract_address::ContractAddress"
-          },
-          {
-            "name": "account",
-            "type": "core::starknet::contract_address::ContractAddress"
-          }
-        ],
-        "outputs": [
-          {
-            "type": "core::integer::u256"
-          }
-        ],
-        "state_mutability": "external"
-      },
-      {
-        "type": "function",
-        "name": "mint_options",
-        "inputs": [
-          {
-            "name": "round_address",
-            "type": "core::starknet::contract_address::ContractAddress"
-          }
-        ],
-        "outputs": [
-          {
-            "type": "core::integer::u256"
-          }
-        ],
-        "state_mutability": "external"
-      },
-      {
-        "type": "function",
-        "name": "exercise_options",
-        "inputs": [
-          {
-            "name": "round_address",
-            "type": "core::starknet::contract_address::ContractAddress"
+            "name": "result",
+            "type": "core::array::Span::<core::felt252>"
           }
         ],
         "outputs": [
@@ -589,5 +433,301 @@ export const ABI = [
         "state_mutability": "external"
       }
     ]
+  },
+  {
+    "type": "struct",
+    "name": "pitch_lake::vault::interface::ConstructorArgs",
+    "members": [
+      {
+        "name": "verifier_address",
+        "type": "core::starknet::contract_address::ContractAddress"
+      },
+      {
+        "name": "eth_address",
+        "type": "core::starknet::contract_address::ContractAddress"
+      },
+      {
+        "name": "option_round_class_hash",
+        "type": "core::starknet::class_hash::ClassHash"
+      },
+      {
+        "name": "alpha",
+        "type": "core::integer::u128"
+      },
+      {
+        "name": "strike_level",
+        "type": "core::integer::i128"
+      },
+      {
+        "name": "round_transition_duration",
+        "type": "core::integer::u64"
+      },
+      {
+        "name": "auction_duration",
+        "type": "core::integer::u64"
+      },
+      {
+        "name": "round_duration",
+        "type": "core::integer::u64"
+      },
+      {
+        "name": "program_id",
+        "type": "core::felt252"
+      },
+      {
+        "name": "proving_delay",
+        "type": "core::integer::u64"
+      }
+    ]
+  },
+  {
+    "type": "constructor",
+    "name": "constructor",
+    "inputs": [
+      {
+        "name": "args",
+        "type": "pitch_lake::vault::interface::ConstructorArgs"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::Deposit",
+    "kind": "struct",
+    "members": [
+      {
+        "name": "account",
+        "type": "core::starknet::contract_address::ContractAddress",
+        "kind": "key"
+      },
+      {
+        "name": "amount",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "account_unlocked_balance_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "vault_unlocked_balance_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::Withdrawal",
+    "kind": "struct",
+    "members": [
+      {
+        "name": "account",
+        "type": "core::starknet::contract_address::ContractAddress",
+        "kind": "key"
+      },
+      {
+        "name": "amount",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "account_unlocked_balance_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "vault_unlocked_balance_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::WithdrawalQueued",
+    "kind": "struct",
+    "members": [
+      {
+        "name": "account",
+        "type": "core::starknet::contract_address::ContractAddress",
+        "kind": "key"
+      },
+      {
+        "name": "bps",
+        "type": "core::integer::u128",
+        "kind": "data"
+      },
+      {
+        "name": "round_id",
+        "type": "core::integer::u64",
+        "kind": "data"
+      },
+      {
+        "name": "account_queued_liquidity_before",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "account_queued_liquidity_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "vault_queued_liquidity_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::StashWithdrawn",
+    "kind": "struct",
+    "members": [
+      {
+        "name": "account",
+        "type": "core::starknet::contract_address::ContractAddress",
+        "kind": "key"
+      },
+      {
+        "name": "amount",
+        "type": "core::integer::u256",
+        "kind": "data"
+      },
+      {
+        "name": "vault_stashed_balance_now",
+        "type": "core::integer::u256",
+        "kind": "data"
+      }
+    ]
+  },
+  {
+    "type": "struct",
+    "name": "pitch_lake::option_round::interface::PricingData",
+    "members": [
+      {
+        "name": "strike_price",
+        "type": "core::integer::u256"
+      },
+      {
+        "name": "cap_level",
+        "type": "core::integer::u128"
+      },
+      {
+        "name": "reserve_price",
+        "type": "core::integer::u256"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::OptionRoundDeployed",
+    "kind": "struct",
+    "members": [
+      {
+        "name": "round_id",
+        "type": "core::integer::u64",
+        "kind": "data"
+      },
+      {
+        "name": "address",
+        "type": "core::starknet::contract_address::ContractAddress",
+        "kind": "data"
+      },
+      {
+        "name": "auction_start_date",
+        "type": "core::integer::u64",
+        "kind": "data"
+      },
+      {
+        "name": "auction_end_date",
+        "type": "core::integer::u64",
+        "kind": "data"
+      },
+      {
+        "name": "option_settlement_date",
+        "type": "core::integer::u64",
+        "kind": "data"
+      },
+      {
+        "name": "pricing_data",
+        "type": "pitch_lake::option_round::interface::PricingData",
+        "kind": "data"
+      }
+    ]
+  },
+  {
+    "type": "struct",
+    "name": "pitch_lake::vault::interface::L1Data",
+    "members": [
+      {
+        "name": "twap",
+        "type": "core::integer::u256"
+      },
+      {
+        "name": "max_return",
+        "type": "core::integer::u128"
+      },
+      {
+        "name": "reserve_price",
+        "type": "core::integer::u256"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::FossilCallbackSuccess",
+    "kind": "struct",
+    "members": [
+      {
+        "name": "l1_data",
+        "type": "pitch_lake::vault::interface::L1Data",
+        "kind": "data"
+      },
+      {
+        "name": "timestamp",
+        "type": "core::integer::u64",
+        "kind": "data"
+      }
+    ]
+  },
+  {
+    "type": "event",
+    "name": "pitch_lake::vault::contract::Vault::Event",
+    "kind": "enum",
+    "variants": [
+      {
+        "name": "Deposit",
+        "type": "pitch_lake::vault::contract::Vault::Deposit",
+        "kind": "nested"
+      },
+      {
+        "name": "Withdrawal",
+        "type": "pitch_lake::vault::contract::Vault::Withdrawal",
+        "kind": "nested"
+      },
+      {
+        "name": "WithdrawalQueued",
+        "type": "pitch_lake::vault::contract::Vault::WithdrawalQueued",
+        "kind": "nested"
+      },
+      {
+        "name": "StashWithdrawn",
+        "type": "pitch_lake::vault::contract::Vault::StashWithdrawn",
+        "kind": "nested"
+      },
+      {
+        "name": "OptionRoundDeployed",
+        "type": "pitch_lake::vault::contract::Vault::OptionRoundDeployed",
+        "kind": "nested"
+      },
+      {
+        "name": "FossilCallbackSuccess",
+        "type": "pitch_lake::vault::contract::Vault::FossilCallbackSuccess",
+        "kind": "nested"
+      }
+    ]
   }
-] as const; 
+] as const;
